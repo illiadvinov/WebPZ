773aa701b2340a32ac5a08ea146e804afbc2183b
The Master <illia.netcore@gmail.com>
2023-04-14 16:56:05 +0200

    Common footer

--- JS/common.js
+++ JS/common.js
@@ -1,7 +1,4 @@
-console.log("Working");
-
 class MyHeader extends HTMLElement {
-
   connectedCallback() {
     this.innerHTML = `
     <a href="#index">
@@ -27,3 +24,28 @@ class MyHeader extends HTMLElement {
 }
 
 customElements.define("my-header", MyHeader);
+
+class MyFooter extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+        <div id="footer">
+      <image src="../images/profstroylogo.png" alt="Лого" id="fl"></image>
+
+      <a href="https://www.instagram.com/profstroy.kh/">
+        <img src="../images/Instagram.png" alt="Instagram logo" id="inst" />
+      </a>
+      <a href="https://www.facebook.com/profstroy.kh">
+        <img src="../images/Facebook.png" alt="Facebook logo" id="face" />
+      </a>
+      <ul class="address">
+        <li>Украина</li>
+        <li>Харьков</li>
+        <li>Улица Эстакадная</li>
+      </ul>
+      <text class="footPr">&copy; 2006-2021 Проф-Строй</text>
+    </div>
+        `;
+  }
+}
+
+customElements.define("my-footer", MyFooter)
